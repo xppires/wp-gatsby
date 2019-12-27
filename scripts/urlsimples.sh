3e2ff15ec720f91ef334b5b1824324d0ed3041e1
Pedro Duarte <xppires@gmail.com>
2019-12-27 02:05:14 +0000

    fixing sw

--- scripts/urlsimples.sh
+++ scripts/urlsimples.sh
@@ -4,6 +4,8 @@ do
  export postpage=${file:6}
   echo  "$file", "$page", "$postpage"
  sed -i "s~$postpage~${page%.html}~g" "$file"
+ sed -i 's~data\\posts~data~g' "$file"
+ sed -i 's/.html\\page/\\page/g' "$file"
   mv "$file" "${file%.html}"
 done
 #aws s3  sync public\posts  s3://www.opassoapasso.com/  --content-type text/html --acl public-read
